6e3ce38c96d50196d16139c247314de70087e880
Twometer <twometer@outlook.de>
2019-02-03 16:09:31 +0100

    Minor fixes and updates

--- index.js
+++ index.js
@@ -8,6 +8,10 @@
         else
             nav.addClass("navbar-expand-y");
 
-        header.css('background-position-y', -(top * 0.25) + "px");
+        // Do not use the slow parallax effect on mobile phones as it produces glitches
+        // because mobile browsers use that "cool" disappearing search bar and that is messing
+        // up the "scrollTop" value.
+        if (window.outerWidth > 768)
+            header.css('background-position-y', -(top * 0.25) + "px");
     });
 })();
