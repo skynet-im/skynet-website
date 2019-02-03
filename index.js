c19783a6c6248dc79c0186e2bc074590eb289809
Twometer <twometer@outlook.de>
2019-02-03 15:25:03 +0100

    Parallax wallpaper

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var nav = $("#main-nav");
-$(document).on("scroll", function (e) {
-    var top = $(this).scrollTop();
-    if (top > 40)
-        nav.removeClass("navbar-expand-y");
-    else
-        nav.addClass("navbar-expand-y");
-});
+(function () {
+    var nav = $("#main-nav");
+    var header = $("#home");
+    $(window).on("scroll", function (e) {
+        var top = $(this).scrollTop();
+        if (top > 40)
+            nav.removeClass("navbar-expand-y");
+        else
+            nav.addClass("navbar-expand-y");
+
+        header.css('background-position-y', -(top * 0.25) + "px");
+    });
+})();
